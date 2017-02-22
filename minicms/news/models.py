a22c8fa19550349ff00ce43522ab16df3d2dba05
shahuqing <shahulian@163.com>
2017-02-22 15:51:39 +0800

    urls添加规则出错前

--- minicms/news/models.py
+++ minicms/news/models.py
@@ -3,6 +3,8 @@ from django.db import models
 #from __future__ import unicode_literals
 #from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+from DjangoUeditor.models import UEditorField
+from django.core.urlresolvers import reverse
 
 class Column(models.Model):
     name = models.CharField('栏目名称', max_length=256)
@@ -11,6 +13,9 @@ class Column(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('column', args=(self.slug,))
 
     class Meta:
         verbose_name = '栏目'
@@ -25,7 +30,7 @@ class Article(models.Model):
     slug = models.CharField('网址', max_length=256, db_index=True)
 
     author = models.ForeignKey('auth.User', blank=True, null=True, verbose_name='作者')
-    content = models.TextField('内容', default='', blank=True)
+    content = UEditorField('内容', height=300, width=1000, default=u'', blank=True, imagePath="uploads/images/", toolbars='besttome', filePath='uploads/files/')
 
     published = models.BooleanField('正式发布', default=True)
 
@@ -34,6 +39,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('column', args=(self.slug,))
 
     class Meta:
         verbose_name = '教程'
--- minicms/news/views.py
+++ minicms/news/views.py
@@ -1,3 +1,14 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse(u'欢迎来到自强学堂学习Django')
+
+def column_detail(request, column_slug):
+    return HttpResponse('column slug: ' + column_slug)
+
+def article_detail(request, article_slug):
+    return HttpResponse('article slug: ' + article_slug)
